70eef51b85dba3eb00a0c56f423912ff68c3366a
hunter <iamhunter32@gmail.com>
2021-12-02 14:00:12 -0500

    added "config" file (lazy)

---
+++ config.py
@@ -0,0 +1,8 @@
+whitelist_file = "whitelist.txt"
+discordmclinklist_file = "discordmclinklist.txt"
+denylist_file = "denyuserid.txt"
+requestlist_file = "requests.txt"
+
+enableWhitelistRemove = True
+
+isSFTPEnabled = False
