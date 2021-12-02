e6365903564428a1859c75edf1f02c81becd3a38
hunter <iamhunter32@gmail.com>
2021-12-02 14:00:26 -0500

    restructured files and added SFTP support

--- fileIO.py
+++ fileIO.py
@@ -1,19 +1,13 @@
-def appendToFile(filename: str, string: str):
-    with open(filename, "a") as f:
-        f.write("\n" + string)
-
-
-def appendExceptTrue(filename, lines, lines_bool):
-    ret = ""
-    while lines_bool.count(True) != 0:
-        idx = lines_bool.index(True)
-        ret = lines.pop(idx)
-        lines_bool.pop(idx)
-    with open(filename, "a") as f:
-        for line in lines:
-            f.write("\n" + line)
-
-    return ret
+from config import *
+from typing import List
+if isSFTPEnabled:
+    from sftpInterface import (
+        appendExceptTrue
+    )
+else:
+    from localInterface import (
+        appendExceptTrue
+    )
 
 
 # for link and requestlist
@@ -56,31 +50,3 @@ def removeFromUIDList(filename: str, userID: int):
     lines_bool: List[bool] = list(map(lambda line: line == str(userID), lines))
 
     return appendExceptTrue(filename, lines, lines_bool)
-
-
-def load_denylist() -> List[str]:
-    with open("denylist.txt", "r") as f:
-        deny_list: List[str] = f.readlines()
-    return deny_list
-
-
-def load_requestlist() -> List[List[str]]:
-    with open("requestlist.txt", "r") as f:
-        request_list: List[List[str]] = list(map(
-            lambda line: line.split(" "), f.readlines()
-        ))
-    return request_list
-
-
-def load_whitelist() -> List[str]:
-    with open("whitelist.txt", "r") as f:
-        white_list: List[str] = f.readlines()
-    return white_list
-
-
-def load_linklist() -> List[List[str]]:
-    with open("discordmclink.txt", "r") as f:
-        link_list = list(map(
-            lambda line: line.split(" "), f.readlines()
-        ))
-    return link_list
